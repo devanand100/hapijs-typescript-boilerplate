68c071ac7bca725a27f0f244706c07e8d40dc507
devanand <gadhvidevanandk@gmail.com>
2024-09-19 22:33:58 +0530

    added service example

---
+++ server/services/user.service.ts
@@ -0,0 +1,11 @@
+import userModel from "../models/user.model";
+
+class UserService {
+  static async getUserById(id: string) {
+    const user = await userModel.findById(id);
+    return user;
+  }
+};
+
+
+export default UserService
